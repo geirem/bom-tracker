96e8bf0b7a1b56f8439cdb8c7aa542a4f9285678
Geir Emblemsvåg <o9c@storebrand.no>
2020-02-27 10:12:56 +0100

    Test-ready, with pom-support.

--- lib/Project.py
+++ lib/Project.py
@@ -1,14 +1,15 @@
 from os import path, environ
+import xml.etree.ElementTree as ET
+from typing import List, Optional, Tuple
 
 
 class Project:
 
+    __POM_NAME = 'pom.xml'
+    __POM_NS = {'maven': 'http://maven.apache.org/POM/4.0.0'}
+
     def __init__(self):
-        if self.__is_maven():
-            (self.__name, self.__version) = self.__project_from_pom()
-        else:
-            self.__name = environ['GITHUB_REPOSITORY']
-            self.__version = environ['GITHUB_SHA']
+        (self.__name, self.__version) = Project.__from_pom() if Project.__is_maven() else Project.__from_vars()
 
     def __str__(self):
         return f'{self.__name}:{self.__version}'
@@ -21,8 +22,29 @@ class Project:
 
     @staticmethod
     def __is_maven() -> bool:
-        return path.exists('pom.xml')
+        return path.exists(Project.__POM_NAME)
 
     @staticmethod
-    def __project_from_pom():
-        return 'foo', 'bar'
+    def __from_pom() -> Tuple[str, str]:
+        tree = ET.parse(Project.__POM_NAME)
+        root = tree.getroot()
+        group_id = Project.__safe_read_xml_value('groupId', root)
+        if group_id is None:
+            group_id = Project.__safe_read_xml_value(root.find('maven:parent', Project.__POM_NS), root)
+        artifact_id = Project.__safe_read_xml_value('artifactId', root)
+        version = Project.__safe_read_xml_value('version', root)
+        if None in (group_id, artifact_id, version):
+            return Project.__from_vars()
+        return f'{group_id}/{artifact_id}', version
+
+    @staticmethod
+    def __safe_read_xml_value(tag: str, element: ET.Element) -> Optional[str]:
+        try:
+            value = element.find(f'maven:{tag}', Project.__POM_NS).text
+        except AttributeError:
+            return None
+        return value
+
+    @staticmethod
+    def __from_vars() -> Tuple[str, str]:
+        return str(environ['GITHUB_REPOSITORY']), str(environ['GITHUB_SHA'])
