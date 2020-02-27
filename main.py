6402bec1654c464a08890007bca717cd9f9bb8a7
Geir Emblemsvåg <o9c@storebrand.no>
2020-02-27 12:36:00 +0100

    Configurable info page and host name.

--- main.py
+++ main.py
@@ -10,20 +10,20 @@ from lib.Reporter import Reporter
 def check_for_blockers(bom_file: str) -> bool:
     bom = Bom(bom_file).parse()
     blocker_file = 'config/blockers.json'
-    info_page = 'https://wiki.stb.intra/some_blocking_info_page.html'
     blocker_checker = BlockerChecker(blocker_file)
     blockers = blocker_checker.check(bom.get_components())
     if blockers:
         print('Some dependencies are vulnerable - failing the build:')
         pprint.pprint(blockers)
-        print(f'See {info_page} for more details and how to solve this.')
+        if 'TRACK_INFO_PAGE' in os.environ:
+            print(f'See {os.environ["TRACK_INFO_PAGE"]} for more details and how to solve this.')
         return False
     return True
 
 
-def report_bom(bom_file: str) -> None:
+def report_bom(track_host: str, bom_file: str) -> None:
     project = Project()
-    reporter = Reporter('http://localhost:8080/api/v1', project)
+    reporter = Reporter(f'{track_host}/api/v1', project)
     reporter.ensure_project_exists()
     reporter.send_bom(bom_file)
 
@@ -36,7 +36,7 @@ def main() -> None:
     if not check_for_blockers(bom_file):
         exit(1)
     try:
-        report_bom(bom_file)
+        report_bom(os.environ['TRACK_HOST'], bom_file)
     except Exception as err:
         print(err)
 
